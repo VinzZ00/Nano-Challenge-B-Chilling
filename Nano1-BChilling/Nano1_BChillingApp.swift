bd587a6f1fe9db7e0330f89fa7b710867b940359
Elvin <elvin.ri39@gmail.com>
2023-03-20 22:12:08 +0700

    Initial Commit

---
+++ Nano1-BChilling/Nano1_BChillingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nano1_BChillingApp.swift
+//  Nano1-BChilling
+//
+//  Created by Elvin Sestomi on 20/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Nano1_BChillingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
